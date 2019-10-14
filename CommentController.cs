da9989f67d377e7e81ce626f71e049c0b268d7a5
yangyingyu07 <54791195+yangyingyu07@users.noreply.github.com>
2019-10-14 15:08:38 +0800

    Add files via upload

---
+++ CommentController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class CommentController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json(DAL.Comment.Instance.GetCount());
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Json(DAL.Comment.Instance.GetWorkCount(id));
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
